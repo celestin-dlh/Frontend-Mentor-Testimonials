7ee6922c9a43cdcb25e4350be84b74e45f84fc6f
Zeeeratul <c.delahaye@agencelafusee.com>
2021-02-16 09:26:30 +0100

    add credit

--- src/App.js
+++ src/App.js
@@ -91,6 +91,10 @@ function App() {
           />
         ))}
       </div>
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/Zeeeratul">Zeeeratul</a>.
+      </div>
     </div>
   )
 }
